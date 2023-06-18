bbf9b9a794204717d47ad8ba97c8f6bbbbf9c3a3
Eugenio <lugezz@gmail.com>
2023-06-18 09:29:03 -0300

    Mongo running

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,3 +14,8 @@ app.listen(port, () => {
     console.log(`Listening on port ${port}!`);
     }
 );
+
+// TODO: Use environment variables
+mongoose.connect('mongodb://artime:artime80@mongo:27017/?authSource=admin')
+    .then(() => console.log('Successfully connected to MongoDB'))
+    .catch(err => console.error('Something went wrong', err));
